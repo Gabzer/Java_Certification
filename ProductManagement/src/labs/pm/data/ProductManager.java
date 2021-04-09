d5cf1df78e46c43a22e9c34d32a893f14eb37474
gabzer <gab.zer.nas@gmail.com>
2021-04-09 09:43:18 -0400

    module 8

--- ProductManagement/src/labs/pm/data/ProductManager.java
+++ ProductManagement/src/labs/pm/data/ProductManager.java
@@ -22,6 +22,7 @@ import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -30,13 +31,14 @@ import java.util.ResourceBundle;
  * @author gabze
  */
 public class ProductManager {
+
     private Locale locale;
     private ResourceBundle resources;
     private DateTimeFormatter dateFormat;
     private NumberFormat moneyFormat;
-    
+
     private Product product;
-    private Review review;
+    private Review[] reviews = new Review[5];
 
     public ProductManager(Locale locale) {
         this.locale = locale;
@@ -44,39 +46,59 @@ public class ProductManager {
         dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).localizedBy(locale);
         moneyFormat = NumberFormat.getCurrencyInstance(locale);
     }
-    
+
     public Product createProduct(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
         product = new Food(id, name, price, rating, bestBefore);
         return product;
     }
-    
+
     public Product createProduct(int id, String name, BigDecimal price, Rating rating) {
         product = new Drink(id, name, price, rating);
         return product;
     }
-    
+
     public Product reviewProduct(Product product, Rating rating, String comments) {
-        review = new Review(rating, comments);
-        this.product = product.applyRating(rating);
+        //  review = new Review(rating, comments);
+        if (reviews[reviews.length - 1] != null) {
+            reviews = Arrays.copyOf(reviews, reviews.length + 5);
+        }
+        int sum = 0, i = 0;
+        boolean reviewed = false;
+        while (i < reviews.length && !reviewed) {
+            if (reviews[i] == null) {
+                reviews[i] = new Review(rating, comments);
+                reviewed = true;
+            }
+            sum += reviews[i].getRating().ordinal(); // gives you the numeric representation of the value
+            i++;
+        }
+
+        this.product = product.applyRating(Rateable.convert(Math.round((float) sum / i)));
+//        this.product = product.applyRating(rating);
         return this.product;
     }
-    
+
     public void printProductReport() {
         StringBuilder txt = new StringBuilder();
-        txt.append(MessageFormat.format(resources.getString("product"), 
-            product.getName(), 
-            moneyFormat.format(product.getPrice()), 
-            product.getRating().getStars(),
-            dateFormat.format(product.getBestBefore())));
+        txt.append(MessageFormat.format(resources.getString("product"),
+                product.getName(),
+                moneyFormat.format(product.getPrice()),
+                product.getRating().getStars(),
+                dateFormat.format(product.getBestBefore())));
         txt.append('\n');
-        if (review != null) {
-            txt.append(MessageFormat.format(resources.getString("review"), 
-                review.getRating().getStars(),
-                review.getComments()));
-        } else {
-            txt.append(resources.getString("no.reviews"));
+        for (Review review : reviews) {
+            if (review == null) {
+                break;
+            }
+            txt.append(MessageFormat.format(resources.getString("review"),
+                    review.getRating().getStars(),
+                    review.getComments()));
+            txt.append('\n');
         }
-        txt.append('\n');
+        if (reviews[0] == null) {
+            txt.append(resources.getString("no.reviews"));
+            txt.append('\n');
+        }
         System.err.println(txt);
     }
 }
